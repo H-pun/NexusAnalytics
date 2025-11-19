b46e43966fdcf4e7670719d1422850c802ae6f92
Idham <idhammultazam7@gmail.com>
2025-11-19 11:02:15 +0700

    refactor(docker): comment out unused service configurations and update service endpoints

--- analytics-ui/next.config.js
+++ analytics-ui/next.config.js
@@ -13,6 +13,38 @@ const resolveAlias = {
 const nextConfig = withLess({
   output: 'standalone',
   staticPageGenerationTimeout: 1000,
+  // Ensure ESM packages from Ant Design ecosystem are transpiled by Next.js
+  transpilePackages: [
+    '@ant-design/icons-svg',
+    'rc-util',
+    'rc-align',
+    'rc-cascader',
+    'rc-checkbox',
+    'rc-collapse',
+    'rc-dialog',
+    'rc-drawer',
+    'rc-dropdown',
+    'rc-field-form',
+    'rc-image',
+    'rc-input',
+    'rc-input-number',
+    'rc-menu',
+    'rc-notification',
+    'rc-pagination',
+    'rc-picker',
+    'rc-progress',
+    'rc-rate',
+    'rc-select',
+    'rc-slider',
+    'rc-steps',
+    'rc-switch',
+    'rc-table',
+    'rc-tabs',
+    'rc-tooltip',
+    'rc-tree',
+    'rc-tree-select',
+    'rc-upload',
+  ],
   compiler: {
     // Enables the styled-components SWC transform
     styledComponents: {
